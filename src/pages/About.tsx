f3357393e83ea0d10006191ea555c2fdbfdff7a4
unknown <tanmay.kaushik451@gmail.com>
2020-08-03 16:50:50 +0530

    ui improved

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const About = () => {
     return (
+        <>
         <div>
-            <h1>Welcome to Github user finder page</h1>
+            <h1>Welcome to Github user finder page :)</h1>
             <small> Creator : https://github.com/tanmaykaushik451</small>
         </div>
+        </>
     )
 }
 
